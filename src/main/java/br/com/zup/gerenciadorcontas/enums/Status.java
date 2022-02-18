3ed1ba248fc6a3e5296d52bdcf7e74a512ffebe0
Savio <pauloluizsavio@gmail.com>
2022-02-18 13:17:08 -0300

    Criar enums

---
+++ src/main/java/br/com/zup/gerenciadorcontas/enums/Status.java
@@ -0,0 +1,7 @@
+package br.com.zup.gerenciadorcontas.enums;
+
+public enum Status {
+    AGUARDANDO,
+    PAGO,
+    VENCIDA
+}
---
+++ src/main/java/br/com/zup/gerenciadorcontas/enums/Tipo.java
@@ -0,0 +1,9 @@
+package br.com.zup.gerenciadorcontas.enums;
+
+public enum Tipo {
+    LUZ,
+    AGUA,
+    COMIDA,
+    LAZER,
+    OUTROS
+}
