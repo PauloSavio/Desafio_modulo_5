7787e91de8b41993562f790b67b0ffe958af270b
Savio <pauloluizsavio@gmail.com>
2022-02-18 13:14:03 -0300

    Primeiro commit

---
+++ src/test/java/br/com/zup/gerenciadorcontas/GerenciadorContasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.gerenciadorcontas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorContasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
