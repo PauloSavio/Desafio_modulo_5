c7b415a2bbce2d06294a18802bf6cbf3db954aa5
Savio <pauloluizsavio@gmail.com>
2022-02-18 13:41:13 -0300

    Criar contaRpository

--- src/main/java/br/com/zup/gerenciadorcontas/Services/ContaService.java
+++ src/main/java/br/com/zup/gerenciadorcontas/Services/ContaService.java
@@ -1,5 +1,6 @@
 package br.com.zup.gerenciadorcontas.Services;
 
+import br.com.zup.gerenciadorcontas.Repositories.ContaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
