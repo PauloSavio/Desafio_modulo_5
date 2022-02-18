b3d2eb3b88ea1a8a5355952579cc4526b1093c11
Savio <pauloluizsavio@gmail.com>
2022-02-18 13:21:01 -0300

    Refatorar model

--- src/main/java/br/com/zup/gerenciadorcontas/models/Conta.java
+++ src/main/java/br/com/zup/gerenciadorcontas/models/Conta.java
@@ -2,15 +2,17 @@ package br.com.zup.gerenciadorcontas.models;
 
 import br.com.zup.gerenciadorcontas.enums.Status;
 import br.com.zup.gerenciadorcontas.enums.Tipo;
+import lombok.Data;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@Data
 public class Conta {
     private String nome;
     private double valor;
-    private Tipo tipoConta;
-    private Status statusConta;
+    private Tipo tipo;
+    private Status status;
     private LocalDate dataDeVencimento;
     private LocalDateTime dataDePagamento;
 }
