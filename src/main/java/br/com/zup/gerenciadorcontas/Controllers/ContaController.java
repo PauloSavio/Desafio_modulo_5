67bfc24981c07426ba0a74811091cf2cf6214791
Savio <pauloluizsavio@gmail.com>
2022-02-18 14:39:37 -0300

    Criar metodo para cadastrar conta na controller

--- src/main/java/br/com/zup/gerenciadorcontas/Controllers/ContaController.java
+++ src/main/java/br/com/zup/gerenciadorcontas/Controllers/ContaController.java
@@ -1,9 +1,13 @@
 package br.com.zup.gerenciadorcontas.Controllers;
 
 import br.com.zup.gerenciadorcontas.Services.ContaService;
+import br.com.zup.gerenciadorcontas.models.Conta;
+import br.com.zup.gerenciadorcontas.models.dtos.CadastroContaDTO;
+import br.com.zup.gerenciadorcontas.models.dtos.RespostaCadastroDTO;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/contas")
@@ -11,5 +15,18 @@ public class ContaController {
 
     @Autowired
     private ContaService contaService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public RespostaCadastroDTO cadastrarConta(@RequestBody CadastroContaDTO cadastroContaDTO) {
+        RespostaCadastroDTO respostaCadastroDTO = modelMapper.map(cadastroContaDTO, RespostaCadastroDTO.class);
+        Conta conta = modelMapper.map(cadastroContaDTO, Conta.class);
+        contaService.cadastrarConta(conta);
+
+        return respostaCadastroDTO;
+    }
 
 }
